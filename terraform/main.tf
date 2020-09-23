196d7bdff9ba19f7674e131046816bde025504a6
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-09-23 11:16:42 +1000

    Reorganized project and added more s3 and dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,40 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "cloud_challenge_vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "cloud_challenge_s3_bucket" {
+  bucket = "chrissayon-cloud-resume"
+  acl    = "private"
+
+  tags = {
+        Name = "Cloud Resume"
+  }
+}
+
+# Create DynamoDB Table
+resource "aws_dynamodb_table" "cloud_challenge_table" {
+  name           = "CloudChallengeTable"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
 }
